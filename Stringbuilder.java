d593d0ef7fc5a6cf18eeddcc647ddc808cc83379
sabha mushtaq <sabhamushtaq191@gmail.com>
2025-09-30 15:01:26 +0530

    concept of interning and StringBuilder

---
+++ Stringbuilder.java
@@ -0,0 +1,17 @@
+public class Stringbuilder {
+public static void print(){
+StringBuilder sb = new StringBuilder("");
+for(char ch ='a' ;ch <= 'z';ch++){
+sb.append(ch);
+
+}
+System.out.println(sb);
+
+}
+
+    public static void main(String args[]){
+print();
+
+
+    }
+}
